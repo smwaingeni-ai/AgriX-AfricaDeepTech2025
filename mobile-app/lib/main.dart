8d11770a012fcfc2a56e30dea95fe5f15ba0a2ff
smwaingeni-ai <smwaingeni.ai@gmail.com>
2025-06-28 22:14:22 +0300

    Update main.dart

--- mobile-app/lib/main.dart
+++ mobile-app/lib/main.dart
@@ -1,1 +1,23 @@
-# mart
+// File: mobile-app/lib/main.dart
+
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(AgriXApp());
+}
+
+class AgriXApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'AgriX AI Advisor',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
